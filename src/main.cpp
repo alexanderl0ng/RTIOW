f8aa76451f89e9fb97dbc921234507b2cbb42ef6
Alex <91468034+alexanderl0ng@users.noreply.github.com>
2026-04-12 20:37:58 +0100

    Completed chapter 6

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,16 @@
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "rtweekend.h"
+
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, float radius, const ray& r) {
-	vec3 oc = center - r.origin();
-	auto a = dot(r.direction(), r.direction());
-	auto b = -2.0 * dot(r.direction(), oc);
-	auto c = dot(oc, oc) - radius * radius;
-	auto discriminant = b * b - 4 * a * c;
-	return (discriminant >= 0);
-}
-
-color ray_color(const ray& r) {
-	if (hit_sphere(point3(0, 0, -1), 0.5, r))
-		return color(1, 0, 0);
+color ray_color(const ray& r, const hittable& world) {
+	hit_record rec;
+	if (world.hit(r, interval(0, infinity), rec)) {
+		return 0.5 * (rec.normal + color(1, 1, 1));
+	}
 
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
@@ -28,6 +23,11 @@ int main() {
 
 	int image_height = int(image_width / aspect_ratio);
 	image_height = (image_height < 1) ? 1 : image_height;
+	
+	hittable_list world;
+
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	auto focal_length = 1.0;
 	auto viewport_height = 2.0;
@@ -53,10 +53,11 @@ int main() {
 			auto ray_direction = pixel_center - camera_center;
 			ray r(camera_center, ray_direction);
 
-			color pixel_color = ray_color(r);
+			color pixel_color = ray_color(r, world);
 			write_color(std::cout, pixel_color);
 		}
 	}
 
 	std::clog << "\rDone.                      \n";
 }
+
---
+++ src/rtweekend.h
@@ -0,0 +1,25 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+using std::make_shared;
+using std::shared_ptr;
+
+const float infinity = std::numeric_limits<float>::infinity();
+const float pi = 3.1415926535897932385;
+
+inline float degrees_to_radians(float degrees) {
+	return degrees * pi / 180.0;
+}
+
+#include "color.h"
+#include "interval.h"
+#include "ray.h"
+#include "vec3.h"
+
+#endif
+

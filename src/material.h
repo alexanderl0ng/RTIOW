1838850e4f7d2b9c7f53c581e96b74ad05bc8dd3
Alex <91468034+alexanderl0ng@users.noreply.github.com>
2026-06-01 10:04:23 +0100

    Completed chapter 11

--- src/material.h
+++ src/material.h
@@ -48,5 +48,39 @@ class metal: public material {
         float fuzz;
 };
 
+class dielectric : public material {
+    public:
+        dielectric(float refraction_index) : refraction_index(refraction_index) {}
+
+        bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+            attenuation = color(1.0, 1.0, 1.0);
+            float ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+            vec3 unit_direction = unit_vector(r_in.direction());
+            float cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+            float sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+
+            bool cannot_refract = ri * sin_theta > 1.0;
+            vec3 direction;
+
+            if (cannot_refract || reflectance(cos_theta, ri) > random_float())
+                direction = reflect(unit_direction, rec.normal);
+            else
+                direction = refract(unit_direction, rec.normal, ri);
+
+            scattered = ray(rec.p, direction);
+            return true;
+        }
+
+    private:
+        float refraction_index;
+
+        static float reflectance (float cosine, float refraction_index) {
+            auto r0 = (1 - refraction_index) / (1 + refraction_index);
+            r0 = r0 * r0;
+            return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+        }
+};
+
 #endif
 
